177807ba4574a859e2db10c9669b4eee11b3c3d3
Dan Cowdry <medtc411@gmail.com>
2022-04-30 12:18:18 -0700

    4/30/22

--- Program.cs
+++ Program.cs
@@ -650,5 +650,29 @@ namespace CodeWars
          return sum / numbers.Length;
       }
 
+      // Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+      // Unfortunately for you, your drive is very bumpy! Given a string showing either flat road("_") or bumps("n"),
+      // work out if you make it home safely. 15 bumps or under, return "Woohoo!", over 15 bumps return "Car Dead".
+      public static string Bumps(string bumps)
+      {
+         int bumpCount = 0;
+         for (int i = 0; i < bumps.Length; i++)
+         {
+            if (bumps[i] == 'n')
+            {
+               bumpCount++;
+            }
+         }
+         if (bumpCount <= 15)
+         {
+            return "Woohoo!";
+         }
+         else
+         {
+            return "Car Dead";
+         }
+      }
+
+
    }
 }
