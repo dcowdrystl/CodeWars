db0888a7ddb8181e163562826619acb63cef9c23
Dan Cowdry <medtc411@gmail.com>
2022-06-22 15:55:07 -0700

    SwitchIt

--- Program.cs
+++ Program.cs
@@ -1348,5 +1348,58 @@ namespace CodeWars
          return result;
 
       }
+
+      //Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+      //Examples
+      //Valid arrays
+      //a = [121, 144, 19, 161, 19, 144, 19, 11]
+      //b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+      //comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+      //a = [121, 144, 19, 161, 19, 144, 19, 11]
+      //b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+      public static bool comp(int[] a, int[] b)
+      {
+         if (a.Length != b.Length)
+         {
+            return false;
+         }
+         else
+         {
+            for (int i = 0; i < a.Length; i++)
+            {
+               if (a[i] != b[i])
+               {
+                  return false;
+               }
+            }
+            return true;
+         }
+      }
+
+      //Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+      public static string SwitchIt(string x)
+      {
+         string result = "";
+         for (int i = 0; i < x.Length; i++)
+         {
+            if (x[i] == 'a')
+            {
+               result += 'b';
+            }
+            else if (x[i] == 'b')
+            {
+               result += 'a';
+            }
+            else
+            {
+               result += x[i];
+            }
+         }
+         return result;
+      }
+      
+
    }
 }
